93322c6844082aca4b28cbf2fb73ff1d60e5ca80
TR_PWA <thawillys@gmail.com>
2026-03-26 12:31:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(self.skipWaiting());
 });
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => new Response('Modo Offline Ativo')));
+  // Uma configuração simples para manter o app online
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('Você está offline, mas o app continua funcionando na aba que já estava aberta!'))
+  );
 });
